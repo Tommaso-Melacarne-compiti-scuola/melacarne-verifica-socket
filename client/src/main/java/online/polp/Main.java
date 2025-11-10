34e4eb7570289f73d4e0518fe2142eeea7a28fce
PolpOnline <git@t0mmy.anonaddy.com>
2025-11-10 10:40:17 +0100

    Add client

---
+++ client/src/main/java/online/polp/Main.java
@@ -0,0 +1,37 @@
+package online.polp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+    static final int PORT = 3000;
+
+    public static void main(String[] args) throws IOException {
+        Socket clientSocket = new Socket("127.0.0.1", PORT);
+
+        System.out.println("Connected to server on port " + PORT);
+
+        Scanner consoleInput = new Scanner(System.in);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+        try {
+            //noinspection InfiniteLoopStatement
+            while (true) {
+                System.out.println(in.readLine());
+                String userInput = consoleInput.nextLine().trim();
+                out.println(userInput);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            clientSocket.close();
+            consoleInput.close();
+        }
+    }
+}
